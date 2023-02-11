0e9b85c836e31175f94357557640526b5b631023
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-02-11 14:33:28 +0900

    Create ダイクストラ.cpp

---
+++ ダイクストラ.cpp
@@ -0,0 +1,16 @@
+void Dijkstra(const Graph& graph, vector<ll>& distances, int startIndex){
+	priority_queue<Pair, vector<Pair>, greater<Pair>> q;
+	q.emplace((distances[startIndex] = 0), startIndex);
+	while (!q.empty()){
+		const long long distance = q.top().first;
+		const int from = q.top().second;
+		q.pop();
+		if (distances[from] < distance)continue;
+		for (const auto& edge : graph[from]){
+			const long long d = (distances[from] + edge.cost);
+			if (d < distances[edge.to]){
+				q.emplace((distances[edge.to] = d), edge.to);
+			}
+		}
+	}
+}
