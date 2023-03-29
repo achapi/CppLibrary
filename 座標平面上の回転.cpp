e4708011ff497fc875456e2ce1e1b9ba4b39d499
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-03-29 17:27:42 +0900

    Update 座標平面上の回転.cpp

--- 座標平面上の回転.cpp
+++ 座標平面上の回転.cpp
@@ -1,5 +1,5 @@
-pair<double,double> affine2D(double r,double x,double y,double a=0,double b=0){
-	double X=cos(r)*(x-a)-sin(r)*(y-b)+a;
-	double Y=sin(r)*(x-a)+cos(r)*(y-b)+b;
+pair<double,double> affine2D(double t,double x,double y,double a=0,double b=0){
+	double X=cos(t)*(x-a)-sin(t)*(y-b)+a;
+	double Y=sin(t)*(x-a)+cos(t)*(y-b)+b;
 	return {X,Y};
 }
