29cfd660331c4bf80e443e2d4007365369ec5161
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-04-23 23:51:01 +0900

    Update 二項係数.cpp

--- 二項係数.cpp
+++ 二項係数.cpp
@@ -1,12 +1,7 @@
-void cmbinit(){
-	for(int i=2;i<5050505;i++){
-		int mod = modint::mod();
-		fact[i] = fact[-1] * i;
-		inv[i] = -inv[mod % i] * (mod / i);
-		factinv[i] = factinv[-1] * inv[-1];
-	}
+vector<mint> fact = {1};
+for (int i = 1; i < 1010101; i++){
+	fact.push_back(fact.back() * (mint)i);
 }
-mint cmb(int n, int r){
-	if(r<0 or n<r)return 0;
-	return fact[n]*factinv[min(r,n-r)]*factinv[n-min(r,n-r)];
-}
+auto cmb = [&](int n,int r){
+	return fact[n] / fact[n - r] / fact[r];
+};
