017382c215aef11f94a6991c65160f9b86fcbcad
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-04-24 00:01:05 +0900

    Update 二項係数.cpp

--- 二項係数.cpp
+++ 二項係数.cpp
@@ -1,6 +1,6 @@
 vector<mint> fact = {1};
 for (int i = 1; i < 1010101; i++){
-	fact.push_back(fact.back() * (mint)i);
+	fact.push_back(fact.back() * mint(i));
 }
 auto cmb = [&](int n,int r){
 	return fact[n] / fact[n - r] / fact[r];
