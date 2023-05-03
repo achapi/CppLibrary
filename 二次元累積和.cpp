eac18e3416ce8c4586624a398cd945a163f6cd1f
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-05-03 18:29:57 +0900

    Create 二次元累積和.cpp

---
+++ 二次元累積和.cpp
@@ -0,0 +1,21 @@
+template<class T>struct cumulative_sum_2d {
+	/* 1-indexed */
+	int h, w;
+	vector<vector<T>> vec;
+	cumulative_sum_2d(vector<vector<T>> a){
+		h = a.size();
+		w = a.front().size();
+		vec.resize(h + 1, vector<T>(w + 1));
+		for (int i = 0; i < h; i++){
+			for (int j = 0; j < w; j++){
+				vec[i + 1][j + 1] = vec[i][j + 1] + vec[i + 1][j] - vec[i][j] + a[i][j];
+			}
+		}
+	}
+	T query(pair<int, int> p1, pair<int, int> p2){
+		if (p1 > p2){
+			swap(p1, p2);
+		}
+		return vec[p2.first][p2.second] - vec[p1.first - 1][p2.second] - vec[p2.first][p1.second - 1] + vec[p1.first - 1][p1.second - 1];
+	}
+};
