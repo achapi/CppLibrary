336d84a6e551524da0557fe35b1e1aa4935293fb
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-05-05 03:30:50 +0900

    Update 二項係数.cpp

--- 二項係数.cpp
+++ 二項係数.cpp
@@ -1,7 +1,10 @@
 vector<mint> fact = {1};
-for (int i = 1; i < 1010101; i++){
+for (int i = 1; i < 10101010; i++){
 	fact.push_back(fact.back() * mint(i));
 }
-auto cmb = [&](int n,int r){
+auto cmb = [&](int n,int r) -> mint{
+	if (n < r){
+		return mint(0);
+	}
 	return fact[n] / fact[n - r] / fact[r];
 };
