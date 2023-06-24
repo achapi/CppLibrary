91af3c7f647e821d6b452f496f061b532b792168
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-06-24 19:33:35 +0900

    Update 二項係数.cpp

--- 二項係数.cpp
+++ 二項係数.cpp
@@ -2,7 +2,7 @@ vector<mint> fact = {1};
 for (int i = 1; i < 10101010; i++){
 	fact.push_back(fact.back() * mint(i));
 }
-auto cmb = [&](int n, int r) -> mint{
+auto cmb = [&](int n, int r) -> mint {
 	if (n < r){
 		return mint(0);
 	}
