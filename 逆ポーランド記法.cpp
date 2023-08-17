159878914549cf76bcf3eac9c7f206b93bb3be78
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-08-18 06:29:24 +0900

    Update 逆ポーランド記法.cpp

--- 逆ポーランド記法.cpp
+++ 逆ポーランド記法.cpp
@@ -2,7 +2,7 @@ vector<string> RPN(string S) {
 	vector<string> A;
 	vector<stack<int>> st(1);
 	/* Only four arithmetic operations */
-	string support = '0123456789+-*/()';
+	string support = "0123456789+-*/()";
 	for (int i = 0; i < (int) S.size(); i++){
 		if (support.find(S[i]) == string::npos){
 			continue;
@@ -23,8 +23,7 @@ vector<string> RPN(string S) {
 				}
 				A.back().push_back(S[i]);
 			} else {
-				debug(st);
-				if ((int) st.back().size() >= 2 and (S[i] == '+' or S[i] == '-')){
+				if ((int) st.back().size() >= 1 and (S[i] == '+' or S[i] == '-')){
 					while (!st.back().empty()){
 						A.resize(A.size() + 1);
 						A.back().push_back(st.back().top());
