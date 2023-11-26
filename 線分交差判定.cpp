d67922a93e87a6693baff22e530275534ea543b2
あちゃぴ <67042875+achapi@users.noreply.github.com>
2023-11-27 00:53:25 +0900

    Update 線分交差判定.cpp

--- 線分交差判定.cpp
+++ 線分交差判定.cpp
@@ -1,12 +1,27 @@
-bool is_intersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4){
-	double s = (x1 - x2) * (y3 - y1) - (y1 - y2) * (x3 - x1);
-	double t = (x1 - x2) * (y4 - y1) - (y1 - y2) * (x4 - x1);
-	if (s * t > 0){
+bool is_intersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){
+	int s = (x1 - x2) * (y3 - y1) - (y1 - y2) * (x3 - x1);
+	int t = (x1 - x2) * (y4 - y1) - (y1 - y2) * (x4 - x1);
+	if ((long long) s * t > 0){
 		return false;
 	}
 	s = (x3 - x4) * (y1 - y3) - (y3 - y4) * (x1 - x3);
 	t = (x3 - x4) * (y2 - y3) - (y3 - y4) * (x2 - x3);
-	if (s * t > 0){
+	if ((long long) s * t > 0){
+		return false;
+	}
+	if ((long long) s * t == 0){
+		if (abs(sqrt((x1 - x3) * (x1 - x3)) + sqrt((x2 - x3) * (x2 - x3)) - sqrt((x1 - x2) * (x1 - x2))) <= 1e-9){
+			return true;
+		}
+		if (abs(sqrt((x1 - x4) * (x1 - x4)) + sqrt((x2 - x4) * (x2 - x4)) - sqrt((x1 - x2) * (x1 - x2))) <= 1e-9){
+			return true;
+		}
+		if (abs(sqrt((x3 - x1) * (x3 - x1)) + sqrt((x4 - x1) * (x4 - x1)) - sqrt((x3 - x4) * (x3 - x4))) <= 1e-9){
+			return true;
+		}
+		if (abs(sqrt((x3 - x2) * (x3 - x2)) + sqrt((x4 - x2) * (x4 - x2)) - sqrt((x3 - x4) * (x3 - x4))) <= 1e-9){
+			return true;
+		}
 		return false;
 	}
 	return true;
