d56197ac188cbb5cbbe668bf8449b491eb27be6d
あちゃぴ <67042875+achapi@users.noreply.github.com>
2024-01-23 02:33:28 +0900

    Update ランレングス圧縮.cpp

--- ランレングス圧縮.cpp
+++ ランレングス圧縮.cpp
@@ -1,3 +1,17 @@
+vector<pair<int, int>> RLE(string A){
+	int N = A.size();
+	vector<pair<int, int>> ret;
+	for (int l = 0; l < N;){
+		int r = l + 1;
+		while (r < N and A[l] == A[r]){
+			r++;
+		};
+		ret.push_back({A[l], r - l});
+		l = r;
+	}
+	return ret;
+}
+
 template<class T>vector<pair<int, int>> RLE(vector<T> A){
 	int N = A.size();
 	vector<pair<int, int>> ret;
