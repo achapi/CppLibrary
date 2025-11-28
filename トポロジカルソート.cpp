20a090e2afc05dd3c1f6f200143570745488865b
あちゃぴ <67042875+achapi@users.noreply.github.com>
2025-11-29 07:41:25 +0900

    Refactor TopologicalSort to use integers and priority queue

--- トポロジカルソート.cpp
+++ トポロジカルソート.cpp
@@ -1,16 +1,28 @@
-template<typename T> vector<T> TopologicalSort(const vector<vector<T>>& g, bool rev = false) {
-	T n = g.size();
-	vector<int> in(n);
-	for (auto& v : g) for (T to : v) in[to]++;
-	priority_queue<T> p1;
-	priority_queue<T, vector<T>, greater<T>> p2;
-	auto* q = rev ? (priority_queue<T>*)&p1 : (priority_queue<T>*)&p2;
-	for (T i = 0; i < n; i++) if (!in[i]) q->push(i);
-	vector<T> r;
-	while (!q->empty()) {
-		T v = q->top(); q->pop();
-		r.push_back(v);
-		for (T to : g[v]) if (--in[to] == 0) q->push(to);
+vector<int> TopologicalSort(const vector<vector<int>>& G, int k = -1){
+	int N = G.size();
+	vector<int> in(N);
+	for (int i = 0; i < N; i++) {
+		for (int to : G[i]) {
+			in[to]++;
+		}
 	}
-	return (int) r.size() == n ? r : vector<T>();
+	priority_queue<int> pq;
+	for (int i = 0; i < N; i++) {
+		if (in[i] == 0) {
+			pq.push(i * k);
+		}
+	}
+	vector<int> r;
+	while (!pq.empty()) {
+		int v = pq.top();
+		pq.pop();
+		r.push_back(v * k);
+		for (int to : G[v * k]) {
+			in[to]--;
+			if (in[to] == 0) {
+				pq.push(to * k);
+			}
+		}
+	}
+	return (int) r.size() == N ? r : vector<int>();
 }
